de849c913354cc6cc12f697e9e7f8a58ed4e50f0
yin.he <1140466863@qq.com>
2018-08-24 12:29:52 +0800

    不常见的单词

--- src/main/java/com/gimc/leetcode/ArrayRotate.java
+++ src/main/java/com/gimc/leetcode/ArrayRotate.java
@@ -9,7 +9,7 @@ public class ArrayRotate {
 
     public static void main(String args[]) {
         int data[][] = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
+        handleTwo(data);
         print(data);
     }
 
