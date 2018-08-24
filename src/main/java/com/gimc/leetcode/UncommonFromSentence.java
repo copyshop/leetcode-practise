98fb440be454a55f211046b11198adc9ee78c8f6
yin.he <1140466863@qq.com>
2018-08-24 20:37:07 +0800

    下一个排列

---
+++ src/main/java/com/gimc/leetcode/UncommonFromSentence.java
@@ -0,0 +1,38 @@
+package com.gimc.leetcode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * author: heyin
+ * date: 2018-08-24
+ * desc: 如果一个单词在其中一个句子中只出现一次，在另一个句子中却没有出现，那么这个单词就是不常见的。
+ * 输入：A = "this apple is sweet", B = "this apple is sour"
+ * 输出：["sweet","sour"]
+ * 输入：A = "apple apple", B = "banana"
+ * 输出：["banana"]
+ */
+public class UncommonFromSentence {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String[] uncommonFromSentences(String A, String B) {
+        Map<String, Integer> count = new HashMap();
+        for (String word : A.split(" "))
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        for (String word : B.split(" "))
+            count.put(word, count.getOrDefault(word, 0) + 1);
+
+        List<String> ans = new LinkedList();
+        for (String word : count.keySet())
+            if (count.get(word) == 1) {
+                ans.add(word);
+            }
+
+        return ans.toArray(new String[ans.size()]);
+    }
+}
