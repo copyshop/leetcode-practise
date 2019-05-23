4078b0ea8539b23cda05ab1eadcd77d9464e90d5
yin.he <1140466863@qq.com>
2019-05-23 16:50:47 +0800

    删除无用的代码

---
+++ src/main/java/com/gimc/leetcode/LongestUnivaluePath.java
@@ -0,0 +1,45 @@
+package com.gimc.leetcode;
+
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * author: heyin
+ * date: 2019-05-23
+ * desc:
+ */
+public class LongestUnivaluePath {
+    
+    int ans;
+    
+    public int longestUnivaluePath(TreeNode root) {
+        ans = 0;
+        arrowLength(root);
+        return ans;
+    }
+    
+    public int arrowLength(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = arrowLength(node.left);
+        int right = arrowLength(node.right);
+        int arrowLeft = 0, arrowRight = 0;
+        if (node.left != null && node.left.val == node.val) {
+            arrowLeft += left + 1;
+        }
+        if (node.right != null && node.right.val == node.val) {
+            arrowRight += right + 1;
+        }
+        ans = Math.max(ans, arrowLeft + arrowRight);
+        return Math.max(arrowLeft, arrowRight);
+    }
+    
+    class TreeNode {
+        
+        int val;
+        
+        TreeNode left;
+        
+        TreeNode right;
+    }
+}
---
+++ src/main/java/com/gimc/leetcode/Test.java
@@ -0,0 +1,9 @@
+package com.gimc.leetcode;
+
+/**
+ * author: heyin
+ * date: 2019-05-23
+ * desc:
+ */
+public class Test {
+}
